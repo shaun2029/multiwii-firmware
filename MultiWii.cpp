88d6c8dbc63faa699ef096c3087e0e1a7ae8237c
Shaun2029 <shauns2029@gmail.com>
2013-09-26 22:12:11 +0100

    Change to failsafe, stop motors starting when they are off.

--- MultiWii.cpp
+++ MultiWii.cpp
@@ -805,7 +805,13 @@ void loop () {
     #if defined(FAILSAFE)
       if ( failsafeCnt > (5*FAILSAFE_DELAY) && f.ARMED) {                  // Stabilize, and set Throttle to specified level
         for(i=0; i<3; i++) rcData[i] = MIDRC;                               // after specified guard time after RC signal is lost (in 0.1sec)
-        rcData[THROTTLE] = conf.failsafe_throttle;
+        
+        // Stop motors starting due to failsafe activation - added by ShaunS
+        // Only set throttle=failsafe_throttle if current throttle > MINCHECK 
+        if (rcData[THROTTLE] > MINCHECK) { //(rcData[THROTTLE] > conf.minthrottle) { 
+          rcData[THROTTLE] = conf.failsafe_throttle;//conf.failsafe_throttle;
+        }
+        
         if (failsafeCnt > 5*(FAILSAFE_DELAY+FAILSAFE_OFF_DELAY)) {          // Turn OFF motors after specified Time (in 0.1sec)
           go_disarm();     // This will prevent the copter to automatically rearm if failsafe shuts it down and prevents
           f.OK_TO_ARM = 0; // to restart accidentely by just reconnect to the tx - you will have to switch off first to rearm
