da3d9a00cf45514b150f2bdb07bb8721c09786ce
Shaun2029 <shauns2029@gmail.com>
2014-03-29 20:18:18 +0000

    Changed MinThrottle for DJI 30A ESCs.

--- config.h
+++ config.h
@@ -60,7 +60,8 @@
     //#define MINTHROTTLE 1120 // for Super Simple ESCs 10A
     //#define MINTHROTTLE 1064 // special ESC (simonk)
     //#define MINTHROTTLE 1050 // for brushed ESCs like ladybird
-    #define MINTHROTTLE 1100 // (*) (**)
+    //#define MINTHROTTLE 1100 // Hobby King 10A with SimonK
+    #define MINTHROTTLE 1200 // for DJI F450 30A ESCs
 
   /****************************    Motor maxthrottle    *******************************/
     /* this is the maximum value for the ESCs at full power, this value can be increased up to 2000 */
