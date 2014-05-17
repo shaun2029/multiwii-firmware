e933bdb63741110bc4123147f78cfa8c3fef3ea8
Shaun2029 <shauns2029@gmail.com>
2014-05-17 14:40:40 +0100

    Disabled RC scaling.

--- config.h
+++ config.h
@@ -401,13 +401,13 @@
                  The values for SCALE_RC_OFFSET, SCALE_RC_GAIN_NUM must be integer values.                 
       */ 
 
-      #define SCALE_RC
-      #define SCALE_RC_GAIN_NUM 1204
-      #define SCALE_RC_OFFSET -250
+      //#define SCALE_RC
+      //#define SCALE_RC_GAIN_NUM 1204
+      //#define SCALE_RC_OFFSET -250
       
       // Uncomment to limit the scaled RC values 
-      #define RC_SCALE_CONSTRAIN_MIN 1000
-      #define RC_SCALE_CONSTRAIN_MAX 2000
+      //#define RC_SCALE_CONSTRAIN_MIN 1000
+      //#define RC_SCALE_CONSTRAIN_MAX 2000
 
 /*************************************************************************************************/
 /*****************                                                                 ***************/
